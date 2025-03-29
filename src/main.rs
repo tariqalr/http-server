85750af8f55c1f5db80acf071e1afa0730b915f9
tariqalr <122934135+tariqalr@users.noreply.github.com>
2025-03-30 00:41:06 +0300

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{prelude::*, BufReader};
-use std::fs;
+use std::{
+	net::{TcpListener, TcpStream},
+	io::{prelude::*, BufReader},
+	fs,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3708").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
 		handle_connection(stream);
     }
 }
@@ -22,13 +23,26 @@ fn handle_connection(mut stream: TcpStream) {
 
 	println!("Request: {http_request:#?}");
 
-	if http_request.first().unwrap() == "GET / HTTP/1.1" {
-		let contents = fs::read_to_string("src/htdocs/index.html").unwrap();
-		let response = format!("HTTP/1.1 200 OK\r\n\r\nContent-Length: {}\r\n\r\n{contents}",contents.len());
-		stream.write_all(response.as_bytes()).unwrap();
-	} else {
-		let response = "HTTP/1.1 404 Not Found";
-		stream.write_all(response.as_bytes()).unwrap();
-	}
 
+	let (status, file) = match &http_request.first().unwrap()[..] {
+		"GET / HTTP/1.1" =>
+			(
+				"HTTP/1.1 200 OK",
+				"src/htdocs/index.html"
+			),
+		_ => 
+			(
+				"HTTP/1.1 404 Not Found",
+				""
+			), 
+	};
+
+	let response = if !file.is_empty() {
+		let contents = fs::read_to_string(file).unwrap();
+		&format!("{status}\r\n\r\nContent-Length: {}\r\n\r\n{contents}",contents.len())
+	} else {
+		status
+	};
+
+	stream.write_all(response.as_bytes()).unwrap();
 }
