3d59c0fc5401038bda43a6c999a072b5d04ff6d3
tariqalr <122934135+tariqalr@users.noreply.github.com>
2025-04-01 01:03:35 +0300

    shutdown + error handling. need to handle image requests

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,70 @@
 use std::thread;
 use std::sync::{mpsc, Arc, Mutex};
+use std::io::{self, Write};
+
+type Job = Box<dyn FnOnce() + Send + 'static>; //thread-safe (Send) heap-alloc (Box) closure that takes ownership of captured data (FnOnce) since threads may outlive calling scope ('static)
 
 pub struct ThreadPool {
-	workers: Vec<Worker>,
-	sender: mpsc::Sender<Job>,
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 impl ThreadPool {
-	pub fn new(size: usize) -> ThreadPool {
-		assert!(size>0);
-		
-		let (sender, receiver) = mpsc::channel();
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0, "Thread count must be >0");
+        
+        let (sender, receiver) = mpsc::channel(); //threadpool sender, shared receiver reference
+        let receiver = Arc::new(Mutex::new(receiver));
+        
+        let workers = (0..size)
+            .map(|id| Worker::new(id, Arc::clone(&receiver)))
+            .collect();
 
-		let receiver = Arc::new(Mutex::new(receiver));
-		
-		let mut workers = Vec::with_capacity(size);
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        ThreadPool { 
+            workers, 
+            sender: Some(sender),
         }
+    }
 
-        ThreadPool { workers, sender }
-	}
-	pub fn execute<F: FnOnce() + Send + 'static>(&self, f: F) {
-		let job = Box::new(f);
-        self.sender.send(job).unwrap();
-	}
+    pub fn execute<F: FnOnce() + Send + 'static>(&self, f: F) {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap(); //called by main thread. send job down channel to be received by thread in pool
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take()); //.take() is necessary to close the channel so threads can join
+
+        for worker in self.workers.drain(..) {
+			println!("Worker {}: joined to main thread", worker.id);
+            worker.thread.join().unwrap();
+        }
+    }
 }
 
 struct Worker {
 	id: usize,
-	thread: thread::JoinHandle<()>,
+    thread: thread::JoinHandle<()>,
 }
 
 impl Worker {
-	fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-		Worker {
-			id, 
-			thread: thread::spawn(move || loop {
-				println!("Worker {id} executing");
-				receiver.lock().expect("Mutex locked by other thread").recv().unwrap();
-			})
-		}
-	}
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+			let mut stdout = io::stdout();
+			writeln!(stdout, "Worker {}: waiting for job", id).unwrap();
+            loop {
+                let job = match receiver.lock().unwrap().recv() { //mutex locked, recv called, mutex unlocked, recv blocks
+                    Ok(job) => job,
+                    Err(_) => {
+                        writeln!(stdout, "Worker {}: channel closed", id).unwrap();
+                        break;
+                    }
+                };
+                writeln!(stdout, "Worker {}: executing job", id).unwrap();
+                job();
+            }
+        });
+        Worker { id, thread }
+    }
 }
-
-type Job = Box<dyn FnOnce() + Send + 'static>;
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use std::{
 use http_server::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:3708").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:3708").expect("Could not bind to port");
 	let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let Ok(stream)=stream else {continue;};
 
 		pool.execute(|| {
 			handle_connection(stream);
@@ -22,32 +22,38 @@ fn handle_connection(mut stream: TcpStream) {
 	let buf_reader = BufReader::new(&stream);
 	let http_request: Vec<_> = buf_reader
 		.lines()
-		.map(|result| result.unwrap())
-		.take_while(|line| !line.is_empty())
+		.map_while(|result| match result {
+			Ok(line) if line.is_empty() => None,
+			Ok(line) => Some(line),
+			Err(_) => None,
+		})
 		.collect();
 
-	println!("Request: {http_request:#?}");
+	if http_request.is_empty() {
+		println!("Connection closed early");
+		return;
+	}
+
+	println!("Request: {http_request:#?}\n");
 
 
-	let (status, file) = match &http_request.first().unwrap()[..] {
-		"GET / HTTP/1.1" =>
-			(
-				"HTTP/1.1 200 OK",
-				"src/htdocs/index.html"
-			),
-		_ => 
-			(
-				"HTTP/1.1 404 Not Found",
-				""
-			), 
+	let (status, file) = match http_request.first().map(|s| s.as_str()).unwrap_or("") {
+		"GET / HTTP/1.1" => ("HTTP/1.1 200 OK","htdocs/index.html"),
+		"GET /cat.html HTTP/1.1" => ("HTTP/1.1 200 OK","htdocs/cat.html"),
+		_ => ("HTTP/1.1 404 Not Found",""), 
 	};
 
 	let response = if !file.is_empty() {
-		let contents = fs::read_to_string(file).unwrap();
-		&format!("{status}\r\nContent-Length: {}\r\nContent-Type: text/html; charset=utf-8\r\n\r\n{contents}",contents.len())
+		match fs::read_to_string(file) {
+            Ok(contents) => &format!(
+                "{status}\r\nContent-Length: {}\r\nContent-Type: text/html; charset=utf-8\r\n\r\n{contents}",
+                contents.len()
+            )[..],
+            Err(_) => "HTTP/1.1 500 Internal Server Error\r\n\r\n",
+        }
 	} else {
 		status
 	};
 
-	stream.write_all(response.as_bytes()).unwrap();
+	stream.write_all(response.as_bytes()).expect("Could not send response");
 }
