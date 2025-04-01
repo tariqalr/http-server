faf56437f09e398a2ac0f50f3579490048110ffc
tariqalr <122934135+tariqalr@users.noreply.github.com>
2025-04-01 15:02:42 +0300

    added images, improved generalisation, added big security risk

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::{
-	net::{TcpListener, TcpStream},
-	io::{prelude::*, BufReader},
-	fs,
+	fs, 
+	io::{prelude::*, BufReader}, 
+	net::{TcpListener, TcpStream}, 
+	path::PathBuf,
 };
 use http_server::ThreadPool;
 
@@ -16,6 +17,13 @@ fn main() {
 			handle_connection(stream);
 		});
     }
+}
+
+enum Response {
+	Text(String),
+	Binary(Vec<u8>),
+	Status,
+	InternalError,
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -36,24 +44,51 @@ fn handle_connection(mut stream: TcpStream) {
 
 	println!("Request: {http_request:#?}\n");
 
+	let mut filepath = PathBuf::new();
 
-	let (status, file) = match http_request.first().map(|s| s.as_str()).unwrap_or("") {
-		"GET / HTTP/1.1" => ("HTTP/1.1 200 OK","htdocs/index.html"),
-		"GET /cat.html HTTP/1.1" => ("HTTP/1.1 200 OK","htdocs/cat.html"),
+	let mut http_request = http_request.first().map(|s| s.as_str()).unwrap_or("").split(" ");
+	let (status, content_type) = match http_request.next().unwrap_or("") {
+		"GET" => {
+			match http_request.next().unwrap_or("").split_once(".") {
+				None => {
+					filepath.push("htdocs/index.html");
+					("HTTP/1.1 200 OK","text/html")
+				},
+				Some((prefix,"jpg")) => {
+					filepath.push(format!("{}.jpg",&prefix[1..])); //big security issue: user can input absolute path/'.' char encoding to traverse filesystem, or can use null terminator to modify target file extension
+					("HTTP/1.1 200 OK","image/jpeg")
+				},
+				Some((prefix,"html")) => {
+					filepath.push(format!("htdocs{}.html",prefix)); //less of a security issue
+					("HTTP/1.1 200 OK","text/html")
+				},
+				_ => ("HTTP/1.1 404 Not Found",""), 
+			}
+		}, 
 		_ => ("HTTP/1.1 404 Not Found",""), 
 	};
 
-	let response = if !file.is_empty() {
-		match fs::read_to_string(file) {
-            Ok(contents) => &format!(
-                "{status}\r\nContent-Length: {}\r\nContent-Type: text/html; charset=utf-8\r\n\r\n{contents}",
-                contents.len()
-            )[..],
-            Err(_) => "HTTP/1.1 500 Internal Server Error\r\n\r\n",
-        }
-	} else {
-		status
+
+	let response = match content_type {
+		"text/html" => {
+			match fs::read_to_string(filepath) {
+				Ok(contents) => Response::Text(format!("{status}\r\nContent-Length: {}\r\nContent-Type: {content_type}; charset=utf-8\r\n\r\n{contents}",contents.len())),
+				Err(_) => Response::InternalError
+			}
+		},
+		"image/jpeg" => {
+			match fs::read(filepath) {
+				Ok(contents) => Response::Binary([format!("{status}\r\nContent-Length: {}\r\nContent-Type: {content_type}; charset=utf-8\r\n\r\n",contents.len()).into_bytes(), contents].concat()),
+				Err(_) => Response::InternalError,
+			}
+		},
+		_ => Response::Status
 	};
 
-	stream.write_all(response.as_bytes()).expect("Could not send response");
+	match response {
+		Response::Text(text) => stream.write_all(text.as_bytes()),
+		Response::Binary(data) => stream.write_all(&data),
+		Response::Status => stream.write_all(status.as_bytes()),
+		Response::InternalError => stream.write_all("HTTP/1.1 500 Internal Server Error\r\n\r\n".as_bytes()),
+	}.expect("Could not send response");
 }
